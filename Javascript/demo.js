53d6faf5e8f2d1437a405dc86f461e551d8e2abf
Deepak Tiwari <ainutiwari483@gmail.com>
2020-09-05 10:24:53 +0530

    This is javascript object

--- Javascript/demo.js
+++
@@ -1,4 +0,0 @@
-
-
-document.write("<h1> <i>External file </i> </h1>");
-document.write("<h2> <i> Telusko Learnings </i> </h2>");
---
+++ Javascript/javascript.js
@@ -0,0 +1,33 @@
+
+		/* Objects in JS */
+		//var Car {
+		//	car_brand: "Tesla",
+		//	car_model: "Model",
+		//	price:35000,
+
+		//	teslaAutoPilot : function()
+		//	{
+		//		document.write("<h2>This car has Auto Pilot </h2>");
+
+		//	}
+
+		//}
+	//	Car.teslaAutoPilot();
+
+
+   //		document.write("<h2> "+Car.car_brand+" </h2>); 
+
+//
+var Car = {
+	var car_brand = "tesla",
+	var car_model = "Model",
+	price : 35000,
+
+	teslaAutoPilot : function()
+	{
+		document.write("<h2> this car has Auto pilot </h2>");
+	}
+}
+document.write("<h2> "+Car.car_brand+" </h2>");
+
+
