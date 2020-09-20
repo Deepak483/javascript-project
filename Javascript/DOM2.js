60644ff14a2f8554ede5e65a2630c4efff65b6a8
Deepak Tiwari <ainutiwari483@gmail.com>
2020-09-20 10:14:36 +0530

    this is for javascript mouseover and mouseout

---
+++ Javascript/DOM2.js
@@ -0,0 +1,18 @@
+    /* DOM manipulation & getelementId method */
+        function fn1()
+        {
+            var rd1 = document.getElementById("rd1");
+            var rd2 = document.getElementById("rd2");
+
+            if (rd1.checked == true)
+                alert("The channel selected is:"+rd1.value);
+            else if(rd2.checked == true)
+                alert("The channel selected is :"+rd2.value);
+            else
+                alert("No channel is selected ");
+
+
+
+
+        }
+
---
+++ Javascript/index.js
@@ -0,0 +1,26 @@
+ let name ='deepak';
+ console.log(name);
+//Cannot be a reserved keyword
+// Should be meaningiful
+// Cannot be start with number (lname)
+// Cannot contain a space or hyphen;
+// Are case-sensitive
+
+let firstName= undefined;
+let FirstName = 'Deepak';
+let person = {  // Object laterals
+
+    name:'Deepak',
+    age:30
+
+};
+console.log(person);
+// Dot notation
+person.name ='Deepak';
+
+
+
+
+ 
+ 
+ 
